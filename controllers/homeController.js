4a29caf5714c28f60b3cf7a01c1e4f9542c8aedd
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-07-23 01:51:43 +0530

    all views set

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,6 +4,26 @@ const home = function(req,res){
     res.render("./../resources/views/index.ejs");
 }
 
+const checkout = function(req,res){
+    res.render("./../resources/views/checkout.ejs");
+}
+
+const cart = function(req,res){
+    res.render("./../resources/views/cart.ejs");
+}
+
+const productDetails = function(req,res){
+    res.render("./../resources/views/product-details.ejs");
+}
+
+const shop = function(req,res){
+    res.render("./../resources/views/shop.ejs");
+}
+
 module.exports = {
-    home
+    home,
+    checkout,
+    cart,
+    productDetails,
+    shop
 }
--- routes/home.js
+++ routes/home.js
@@ -5,18 +5,10 @@ var router = express.Router();
 var homeController = require("./../controllers/homeController.js");
 
 router.get('/', (req,res)=>{homeController.home(req,res);});
-
-
-
-/*======================
-        DELETE
-======================*/
-router.get('/another', (req,res)=>{
-    res.send("This is another page");
-});
-/*===========================
-        TILL HERE
-===========================*/
-
+router.get('/index', (req,res)=>{homeController.home(req,res);});
+router.get('/checkout', (req,res)=>{homeController.checkout(req,res);});
+router.get('/cart', (req,res)=>{homeController.cart(req,res);});
+router.get('/product-details', (req,res)=>{homeController.productDetails(req,res);});
+router.get('/shop', (req,res)=>{homeController.shop(req,res);});
 
 module.exports = router;
