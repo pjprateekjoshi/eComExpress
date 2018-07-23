41e80e52ce50468dd569215196759a4f4ca8c069
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-07-23 21:15:04 +0530

    index link remove

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,6 @@ var router = express.Router();
 var homeController = require("./../controllers/homeController.js");
 
 router.get('/', (req,res)=>{homeController.home(req,res);});
-router.get('/index', (req,res)=>{homeController.home(req,res);});
 router.get('/checkout', (req,res)=>{homeController.checkout(req,res);});
 router.get('/cart', (req,res)=>{homeController.cart(req,res);});
 router.get('/product-details', (req,res)=>{homeController.productDetails(req,res);});
